b461de378cf9ceca07afe211c5a28d0b7278f15a
ankit <sindeadly25@gmail.com>
2021-10-12 21:17:38 +0530

    Navbar done

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCPFQyskVhkuhO1p0CgwGgaXfN7WrSk9hU",
+  authDomain: "clone-7257a.firebaseapp.com",
+  projectId: "clone-7257a",
+  storageBucket: "clone-7257a.appspot.com",
+  messagingSenderId: "655593307283",
+  appId: "1:655593307283:web:4752842d81a9b06032e166",
+  measurementId: "G-C7VY863GX5",
+};
